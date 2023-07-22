36e589d06aaa1189dd76cf3cc0da41a45c05ec63
saloxiddinnuridinov <nuridinovsaloxiddin@gmail.com>
2023-07-22 16:57:37 +0500

    Add: paket

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/chat', function () {
-    broadcast(
-        new \App\Events\CreateChat("Hello Programmer"));
-});
